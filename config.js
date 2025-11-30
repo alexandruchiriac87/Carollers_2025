765c91e1991a718818af5521521229abf6da519d
alexandruchiriac87 <alexandruchiriac2005@gmail.com>
2025-11-30 22:15:29 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+// CONFIGURAȚIE - MODIFICĂ AICI
+window.APP_CONFIG = {
+    ADMIN_PASSWORD: "carol2025",
+    FIREBASE_CONFIG: {
+        apiKey: "AIzaSyBFg-D3v6iYbaqDFs8cZBIsPeKVkByrmM0",
+        authDomain: "carollers2025-9f727.firebaseapp.com",
+        databaseURL: "https://carollers2025-9f727-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "carollers2025-9f727",
+        storageBucket: "carollers2025-9f727.firebasestorage.app",
+        messagingSenderId: "611918656458",
+        appId: "1:611918656458:web:c3688cfbc6813cd8c21533"
+    }
+};
